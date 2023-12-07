01e0105992f86db3c5fdff972fd69fecb2965c49
Siddhraj47 <145597969+Siddhraj47@users.noreply.github.com>
2023-12-07 11:17:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+letoutput = document.getElementById("output");
+document.getElementById("btn").addEventListener("click", btnClicked);
+function btnclicked() {
+    let randNum = Math,reandom{};
+    let question = document.getElemetnById("search-in").value.toLowerCase(); }
+If (question === "is javascript awesome") {
+    outputAnswer.innerHTML = "Yes, obivously"}
+ else if (question === "Does a magic 8 ball actually work"){
+    outputAnswer,innerHTML ="How dare you doubt me"
+ }
+
+
